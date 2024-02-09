3b1aae647990d88c8e99d91b28648171e67235e8
Abdulqadir Mohammed <133521025+abdulqadirmsingi@users.noreply.github.com>
2024-02-09 19:17:58 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 import os
 
-mytext = "hud is a student from UDSM and he studies computer science"
+mytext = "Abdulqadir is a student from UDSM and he studies computer science"
 audio = gTTS(text=mytext, lang="en", slow=False)
 
 audio.save("example.mp3")
